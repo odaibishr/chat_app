8bb59f8b9c84c02425188e67d7c6699e5a0afcf3
Odai Bishr <odaibishr@gmail.com>
2025-08-22 18:57:46 +0300

    migrate LoginPage to Bloc with LoginCubit and wrap app with BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:scholar_chat/pages/chat_page.dart';
+import 'package:scholar_chat/pages/cubits/login_cubit/login_cubit.dart';
 import 'package:scholar_chat/pages/login_page.dart';
 import 'package:scholar_chat/pages/resgister_page.dart';
 
 import 'firebase_options.dart';
 
-void main() async  {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(ScholarChat());
+  runApp(const ScholarChat());
 }
 
 class ScholarChat extends StatelessWidget {
@@ -19,13 +21,16 @@ class ScholarChat extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-       LoginPage.id: (context) => LoginPage(),
-        RegisterPage.id: (context) => RegisterPage(),
-        ChatPage.id : (context) => ChatPage()
-      },
-      initialRoute: LoginPage.id,
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: MaterialApp(
+        routes: {
+          LoginPage.id: (context) => LoginPage(),
+          RegisterPage.id: (context) => RegisterPage(),
+          ChatPage.id: (context) => ChatPage()
+        },
+        initialRoute: LoginPage.id,
+      ),
     );
   }
 }
